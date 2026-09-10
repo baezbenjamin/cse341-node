5860da8a2c95889beea15186c41b7c2b61e6fcbb
baezbenjamin <baezbenjamin03@gmail.com>
2026-09-10 03:04:02 -0300

    First Version Stretch Challenge Not Working

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+
 const professionalRoutes = require('./routes/professional');
 
 const app = express();
@@ -14,5 +17,13 @@ app.use((req, res, next) => {
 
 app.use('/professional', professionalRoutes);
 
-app.listen(8080);
-console.log("Oiko hina la server")
+const port = process.env.PORT || 8080;
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log("Oiko hina la server")
+    }
+})
