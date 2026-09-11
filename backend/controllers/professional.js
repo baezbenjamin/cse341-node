04f1afa48f40259fbb70db33168e3f357420c9d4
baezbenjamin <baezbenjamin03@gmail.com>
2026-09-11 01:55:30 -0300

    Latest Version Stretch Challegence Yes Working

--- backend/controllers/professional.js
+++ backend/controllers/professional.js
@@ -1,7 +1,7 @@
 const mongodb = require('../db/connect');
 
 const getData = async (req, res, next) => {
-  const result = await mongodb.getDb().db('test_database').collection('db_collection').find();
+  const result = await mongodb.getDb().db('test_database').collection('test_collection').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
